4c5430a2b85b6c47a5e2ebdf7c6a057e8f72fbde
Michael Madden <mikemadden42@icloud.com>
2019-06-27 21:38:15 -0500

    Generate struct with gojson

--- main.go
+++ main.go
@@ -6,83 +6,78 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strconv"
-	"time"
 )
 
 type discuss struct {
-	Users []struct {
-		ID             int    `json:"id"`
-		Username       string `json:"username"`
-		Name           string `json:"name"`
-		AvatarTemplate string `json:"avatar_template"`
-	} `json:"users"`
 	PrimaryGroups []struct {
-		ID           int    `json:"id"`
-		Name         string `json:"name"`
-		FlairURL     string `json:"flair_url"`
 		FlairBgColor string `json:"flair_bg_color"`
 		FlairColor   string `json:"flair_color"`
+		FlairURL     string `json:"flair_url"`
+		ID           int64  `json:"id"`
+		Name         string `json:"name"`
 	} `json:"primary_groups"`
 	TopicList struct {
 		CanCreateTopic bool        `json:"can_create_topic"`
-		MoreTopicsURL  string      `json:"more_topics_url"`
 		Draft          interface{} `json:"draft"`
 		DraftKey       string      `json:"draft_key"`
-		DraftSequence  int         `json:"draft_sequence"`
-		PerPage        int         `json:"per_page"`
+		DraftSequence  interface{} `json:"draft_sequence"`
+		MoreTopicsURL  string      `json:"more_topics_url"`
+		PerPage        int64       `json:"per_page"`
 		TopTags        []string    `json:"top_tags"`
 		Topics         []struct {
-			ID                 int           `json:"id"`
-			Title              string        `json:"title"`
-			FancyTitle         string        `json:"fancy_title"`
-			Slug               string        `json:"slug"`
-			PostsCount         int           `json:"posts_count"`
-			ReplyCount         int           `json:"reply_count"`
-			HighestPostNumber  int           `json:"highest_post_number"`
-			ImageURL           interface{}   `json:"image_url"`
-			CreatedAt          time.Time     `json:"created_at"`
-			LastPostedAt       time.Time     `json:"last_posted_at"`
-			Bumped             bool          `json:"bumped"`
-			BumpedAt           time.Time     `json:"bumped_at"`
-			Unseen             bool          `json:"unseen"`
-			Pinned             bool          `json:"pinned"`
-			Unpinned           interface{}   `json:"unpinned"`
-			Excerpt            string        `json:"excerpt,omitempty"`
-			Visible            bool          `json:"visible"`
-			Closed             bool          `json:"closed"`
-			Archived           bool          `json:"archived"`
-			Bookmarked         interface{}   `json:"bookmarked"`
-			Liked              interface{}   `json:"liked"`
-			Tags               []interface{} `json:"tags"`
-			Views              int           `json:"views"`
-			LikeCount          int           `json:"like_count"`
-			HasSummary         bool          `json:"has_summary"`
-			Archetype          string        `json:"archetype"`
-			LastPosterUsername string        `json:"last_poster_username"`
-			CategoryID         int           `json:"category_id"`
-			PinnedGlobally     bool          `json:"pinned_globally"`
-			FeaturedLink       interface{}   `json:"featured_link"`
-			HasAcceptedAnswer  bool          `json:"has_accepted_answer"`
-			VoteCount          interface{}   `json:"vote_count"`
-			CanVote            bool          `json:"can_vote"`
-			UserVoted          bool          `json:"user_voted"`
+			Archetype          string      `json:"archetype"`
+			Archived           bool        `json:"archived"`
+			Bookmarked         interface{} `json:"bookmarked"`
+			Bumped             bool        `json:"bumped"`
+			BumpedAt           string      `json:"bumped_at"`
+			CanVote            bool        `json:"can_vote"`
+			CategoryID         int64       `json:"category_id"`
+			Closed             bool        `json:"closed"`
+			CreatedAt          string      `json:"created_at"`
+			Excerpt            string      `json:"excerpt"`
+			FancyTitle         string      `json:"fancy_title"`
+			FeaturedLink       interface{} `json:"featured_link"`
+			HasAcceptedAnswer  bool        `json:"has_accepted_answer"`
+			HasSummary         bool        `json:"has_summary"`
+			HighestPostNumber  int64       `json:"highest_post_number"`
+			ID                 int64       `json:"id"`
+			ImageURL           string      `json:"image_url"`
+			LastPostedAt       string      `json:"last_posted_at"`
+			LastPosterUsername string      `json:"last_poster_username"`
+			LikeCount          int64       `json:"like_count"`
+			Liked              interface{} `json:"liked"`
+			Pinned             bool        `json:"pinned"`
+			PinnedGlobally     bool        `json:"pinned_globally"`
 			Posters            []struct {
-				Extras         interface{} `json:"extras"`
-				Description    string      `json:"description"`
-				UserID         int         `json:"user_id"`
-				PrimaryGroupID int         `json:"primary_group_id"`
+				Description    string `json:"description"`
+				Extras         string `json:"extras"`
+				PrimaryGroupID int64  `json:"primary_group_id"`
+				UserID         int64  `json:"user_id"`
 			} `json:"posters"`
-			LastReadPostNumber int `json:"last_read_post_number,omitempty"`
-			Unread             int `json:"unread,omitempty"`
-			NewPosts           int `json:"new_posts,omitempty"`
-			NotificationLevel  int `json:"notification_level,omitempty"`
+			PostsCount int64       `json:"posts_count"`
+			ReplyCount int64       `json:"reply_count"`
+			Slug       string      `json:"slug"`
+			Tags       []string    `json:"tags"`
+			Title      string      `json:"title"`
+			Unpinned   interface{} `json:"unpinned"`
+			Unseen     bool        `json:"unseen"`
+			UserVoted  interface{} `json:"user_voted"`
+			Views      int64       `json:"views"`
+			Visible    bool        `json:"visible"`
+			VoteCount  interface{} `json:"vote_count"`
 		} `json:"topics"`
 	} `json:"topic_list"`
+	Users []struct {
+		AvatarTemplate string `json:"avatar_template"`
+		ID             int64  `json:"id"`
+		Name           string `json:"name"`
+		Username       string `json:"username"`
+	} `json:"users"`
 }
 
 func main() {
-	fmt.Printf("%-33s %s\n", "Date", "Title")
-	fmt.Printf("%-33s %s\n", "====", "=====")
+	fmt.Printf("%-24s %s\n", "Date", "Title")
+	fmt.Printf("%-24s %s\n", "====", "=====")
 	for i := 0; i < 10; i++ {
 		unanswered(i)
 	}
@@ -101,7 +96,7 @@ func unanswered(page int) {
 
 	for _, topic := range responseObject.TopicList.Topics {
 		if topic.PostsCount == 1 {
-			fmt.Printf("%33s %s\n", topic.CreatedAt, topic.Title)
+			fmt.Printf("%24s %s\n", topic.CreatedAt, topic.Title)
 		}
 	}
 }
